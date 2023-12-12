eb5058d2340c27925e6ee1cce352012de420ba94
Sisol <85529362+Eselvire@users.noreply.github.com>
2023-12-12 10:30:05 +0100

    add zod schemas, refactor db to use cached prepared statements

--- tools/db.ts
+++ tools/db.ts
@@ -1,5 +1,6 @@
 import { Database } from "bun:sqlite";
 import { isFiltered } from "./filtering/utils";
+import { HolderContent } from "./schemas";
 
 // TODO: might want to split it into multiple dbs to not have to re-up the whole thing each time unrelated data is added
 const DB_PATH = "./databases/creations.sqlite"
@@ -9,16 +10,14 @@ const db = new Database(DB_PATH, { create: true });
 // Sorry for all the boilerplate
 
 db.query(`create table if not exists sprites (id TEXT PRIMARY KEY, value BLOB );`).run();
-const storeBlobStmt = db.query(`insert or ignore into sprites VALUES (?, ?);`);
 const storeBlob_ = (id: string, blob: Uint8Array) => {
     if (isFiltered(id)) return;
 
-    storeBlobStmt.run(id, blob);
+    db.query(`insert or ignore into sprites VALUES (?, ?);`).run(id, blob);
 }
 export const storeBlob = async (id: string, blob: Blob) => storeBlob_(id, new Uint8Array(await blob.arrayBuffer()));
-const getSpriteStmt = db.prepare(`SELECT value FROM sprites WHERE id = ?`)
-const getSprite_ = (id: string) => getSpriteStmt.get(id) as { value: Uint8Array } | null;
-const getSprite = (id: string): Blob | null => {
+const getSprite_ = (id: string) => db.prepare(`SELECT value FROM sprites WHERE id = ?`).get(id) as { value: Uint8Array } | null;
+export const getSprite = (id: string): Blob | null => {
     const res = getSprite_(id);
     if (!res) return null;
 
@@ -31,38 +30,51 @@ export const writeSpriteToFile = (id: string, path: string) => {
     Bun.write(path, res.value);
     return true;
 }
-const deleteSpriteStmt = db.prepare(`DELETE * FROM sprites WHERE id = ?`)
-export const deleteSprite = (id: string) => deleteSpriteStmt.run(id);
+export const deleteSprite = (id: string) => db.prepare(`DELETE FROM sprites WHERE id = ?`).run(id);
+
+
 
 
 db.query(`create table if not exists defs (id TEXT PRIMARY KEY, value JSON);`).run();
-const storeDefStmt = db.query(`insert or ignore into defs VALUES (?, ?);`);
 export const storeDef = (id: string, def: any) => {
     if (isFiltered(id)) return;
 
-    storeDefStmt.run(id, def);
+    db.query(`insert or ignore into defs VALUES (?, ?);`).run(id, def);
 }
-const hasStmt = db.query(`select 1 from defs WHERE id = ? LIMIT 1;`);
-export const hasDef = (id: string) => hasStmt.get(id) !== null;
-const getAllIdsStmt = db.query("SELECT id FROM defs;");
-export const getAllIds = () => getAllIdsStmt.values().flat() as string[];
-const deleteDefStmt = db.prepare(`DELETE * FROM defs WHERE id = ?`)
-export const deleteDef = (id: string) => deleteDefStmt.run(id);
+export const hasDef = (id: string) => db.query(`select 1 from defs WHERE id = ? LIMIT 1;`).get(id) !== null;
+export const getDef = (id: string) => JSON.parse((db.query(`SELECT value FROM defs WHERE id = ?`).get(id) as { value: string }).value);
+export const deleteDef = (id: string) => db.query(`DELETE FROM defs WHERE id = ?`).run(id);
+export const getAllIds = () => db.query("SELECT id FROM defs;").values().flat() as string[];
 
 
 db.query(`create table if not exists queue ( id TEXT PRIMARY KEY );`).run();
-const addToQueueStmt = db.query(`insert or ignore into queue VALUES (?);`);
 export const addToQueue = (id: string) => {
     if (isFiltered(id)) return;
+    if (hasDef(id)) return;
 
-    addToQueueStmt.run(id);
+    db.query(`insert or ignore into queue VALUES (?);`).run(id);
 }
-const getQueuePageStmt = db.query(`SELECT id FROM queue LIMIT 50 OFFSET ?;`);
-export let getQueuePage = (page: number) => getQueuePageStmt.values( 50 * page ) as [string][];
-const deleteQueueStmt = db.prepare(`DELETE * FROM queue WHERE id = ?`)
-export const deleteQueue = (id: string) => deleteQueueStmt.run(id);
+export const getQueuePage = (page: number) => (db.query(`SELECT id FROM queue LIMIT 50 OFFSET ?;`).values( 50 * page ) as [string][]).flat();
+export const deleteQueue = (id: string) => db.query(`DELETE FROM queue WHERE id = ?`).run(id);
 
 
 db.query(`create table if not exists errors_creations (id TEXT PRIMARY KEY, type TEXT, statusCode INTEGER);`).run();
 const storeError = db.query(`insert or ignore into errors_creations VALUES (?, ?, ?);`);
 export const storeErrorCreation = (id: string, type: "sprite" | "def", statusCode: number | null) => storeError.run(id, type, statusCode);
+
+
+
+
+db.query(`create table if not exists bodymotions (id TEXT PRIMARY KEY, contents JSON);`).run();
+export const hasMotionData = (id: string) => db.query(`select 1 from bodymotions WHERE id = ? LIMIT 1;`).get(id) !== null;
+export const storeBodyMotions = (id: string, contents: null | string[]) => {
+    db.query("INSERT INTO bodymotions VALUES (?, ?)").run(id, contents === null ? null : JSON.stringify(contents))
+}
+
+db.query(`create table if not exists holdercontents (id TEXT PRIMARY KEY, contents JSON);`).run();
+export const hasHolderData = (id: string) => db.query(`select 1 from holdercontents WHERE id = ? LIMIT 1;`).get(id) !== null;
+export const storeHolderContent = (id: string, contents: HolderContent[] | null) => {
+    db.query("INSERT INTO holdercontents VALUES (?, ?)").run(id, contents === null ? null : JSON.stringify(contents))
+}
+
+db.query(`create table if not exists multicontents (id TEXT PRIMARY KEY, contents JSON);`).run();
---
+++ tools/schemas.ts
@@ -0,0 +1,41 @@
+import { z } from 'zod'
+
+export const MongoId = z.string().regex(/[a-f0-9]{24}/i)
+
+export const CreationDef = z.object({
+    id: MongoId,
+    base: z.string(),
+    name: z.string().optional(),
+
+    props: z.object({
+        emitsId: MongoId.optional(),
+        motionId: MongoId.optional(),
+        environmentId: MongoId.optional(),
+        getId: MongoId.optional(),
+        hasId: MongoId.optional(),
+        holdableId: MongoId.optional(),
+        wearableId: MongoId.optional(),
+        thingRefs: MongoId.optional(),
+    }).optional()
+})
+export type CreationDef = z.infer<typeof CreationDef>
+
+export const Motions = z.object({ ids: MongoId.array().nullable(), midpoint: z.coerce.number().nullable() })
+export const HolderContent = z.object({
+    _id: MongoId,
+    itemId: MongoId,
+    flip: z.coerce.number(),
+    rot: z.coerce.number(),
+    x: z.coerce.number(),
+    y: z.coerce.number(),
+    z: z.coerce.number(),
+    pageNo: z.coerce.number(),
+})
+export type HolderContent = z.infer<typeof HolderContent>
+export const HolderContents = z.object({ isCreator: z.boolean(), contents: HolderContent.array() })
+export const MultiContents = z.object({ isCreator: z.boolean(), data: z.object({
+    _id: MongoId,
+    multithingId: MongoId,
+    itemProps: z.object({id: MongoId, state: z.coerce.number().optional() }).array(),
+    // More but it's not relevant
+}) })
